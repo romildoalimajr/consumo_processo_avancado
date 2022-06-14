7ceb9e9194548db615dd171358caa486d2cb8859
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-06-13 22:38:13 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/consumo_processo_avancado/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.consumo_processo_avancado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,189 @@
+import 'package:consumo_processo_avancado/Post.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _urlBase = "https://jsonplaceholder.typicode.com";
+
+
+  Future<List<Post>> _recuperarPostagens() async{
+
+    http.Response response = await http.get(Uri.parse(_urlBase + "/posts"));
+    var dadosJson = json.decode( response.body );
+
+    List<Post> postagens = [];
+
+    for(var post in dadosJson){
+      print("post.: " + post["title"]);
+      Post p = Post(post["userId"], post["id"], post["title"],post["body"]);
+      postagens.add( p );
+    }
+    return postagens;
+  }
+
+  _post() async{
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "id": null,
+          "title": "Titulo",
+          "body": "Corpo da Postagem"
+        }
+    );
+    http.Response response = await http.post(Uri.parse(
+      _urlBase + "/posts"
+    ),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta.: ${response.statusCode}");
+    print("resposta.: ${response.body}");
+  }
+  _put() async{
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "id": null,
+          "title": "Titulo alterado",
+          "body": "Corpo da Postagem alterada"
+        }
+    );
+    http.Response response = await http.put(Uri.parse(
+        _urlBase + "/posts/2"
+    ),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta.: ${response.statusCode}");
+    print("resposta.: ${response.body}");
+  }
+  _patch() async{
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "body": "Só vai alterar uma parte da Postagem "
+        }
+    );
+    http.Response response = await http.patch(Uri.parse(
+        _urlBase + "/posts/2"
+    ),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta.: ${response.statusCode}");
+    print("resposta.: ${response.body}");
+  }
+  _delete() async{
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "body": "Só vai alterar uma parte da Postagem "
+        }
+    );
+    http.Response response = await http.delete(Uri.parse(
+        _urlBase + "/posts/2"
+    ),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo,
+    );
+    print("resposta.: ${response.statusCode}");
+    print("resposta.: ${response.body}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de Serviço Avançado"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                    child: Text("Salvar"),
+                    onPressed: _post,
+                ),
+                RaisedButton(
+                  child: Text("Atualizar"),
+                  onPressed: _put,
+                ),
+                RaisedButton(
+                  child: Text("Patch"),
+                  onPressed: _patch,
+                ),
+                RaisedButton(
+                  child: Text("Remover"),
+                  onPressed: _delete,
+                ),
+              ],
+            ),
+
+           Expanded(
+               child:  FutureBuilder<List<Post>>(
+
+                 future: _recuperarPostagens(),
+                 builder: (context, snapshot){
+
+                   switch(snapshot.connectionState){
+                     case ConnectionState.none:
+                     case ConnectionState.waiting:
+                       return Center(
+                         child: CircularProgressIndicator(),
+                       );
+                       break;
+                     case ConnectionState.active:
+                     case ConnectionState.done:
+                       print("conexao done");
+                       if( snapshot.hasError ){
+                         print("lista: Erro ao carregar");
+                       }else{
+                         print("lista: carregou");
+                         return ListView.builder(
+                           itemCount: snapshot.data!.length,
+                           itemBuilder: (context, index){
+                             List<Post> lista = snapshot.data!;
+                             Post post = lista[index];
+
+                             return ListTile(
+                               title: Text(post.title),
+                               subtitle: Text(post.id.toString()),
+                             );
+                           },
+                         );
+                       }
+                       break;
+                   }
+                   return Center(
+                     //child: Text(),
+                   );
+                 },
+               ),
+           ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
